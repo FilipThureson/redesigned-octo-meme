581ced97fcdb22b306da0798e3a613b6e24f2be7
Filip Thuresson <60510154+FilipThureson@users.noreply.github.com>
2021-12-26 22:48:11 +0100

    First login Progress

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,19 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-     /**
+    /**
      * Show a list of all of the application's users.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        $users = DB::select('select * from users where active = ?', [1]);
+        $users = DB::select('select * from users');
 
-        return view('user.index', ['users' => $users]);
+        return var_dump($users);
+    }
+
+    public function login()
+    {
+        return view("login");
+    }
+    public function loginAuth()
+    {
+        return "authtesting...";
     }
 }
