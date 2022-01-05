37580ff33b574577dd3e0201649e24aee8916482
Filip Thuresson <60510154+FilipThureson@users.noreply.github.com>
2022-01-05 19:04:35 +0100

    migration

---
+++ database/migrations/2022_01_05_180412_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->string('img');
+            $table->integer('user_id');
+            $table->integer('likes')->nullable()->default(0);
+            $table->timestamp('uploaded_at')->useCurrent();
+            $table->string('caption');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
